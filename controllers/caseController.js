1a52e136253d371c8cee5110609e7d58f3762357
ramazandautbek <ramazandautbek04@gmail.com>
2025-03-15 00:33:28 +0500

    added searchCase func

--- controllers/caseController.js
+++ controllers/caseController.js
@@ -1,58 +1,60 @@
 const Case = require('../models/case');
+const {body} = require("express-validator");
 
 exports.createCase = async (req, res) => {
     try {
-        const { title, description } = req.body;
+        const {title, description} = req.body;
+        const userId = req.user._id;
 
         if (!title) {
-            return res.status(400).json({ message: "Title is required" });
+            return res.status(400).json({message: "Title is required"});
         }
 
-        const newCase = new Case({ title, description });
+        const newCase = new Case({title, description, userId});
         const savedCase = await newCase.save();
 
         return res.status(201).json({success: true, savedCase});
     } catch (err) {
-        return res.status(500).json({ success: false, message: err.message });
+        return res.status(500).json({success: false, message: err.message});
     }
 };
 
 exports.updateCase = async (req, res) => {
     const {title, description} = req.body;
 
-    try{
+    try {
         const updateFields = {title, description}
-        const updateCase = await Case.findByIdAndUpdate(req.params.id , updateFields , {new: true})
-        if(!updateCase){
-            return res.status(404).json({success: false, message:"No such case"});
+        const updateCase = await Case.findByIdAndUpdate(req.params.id, updateFields, {new: true})
+        if (!updateCase) {
+            return res.status(404).json({success: false, message: "No such case"});
         }
-        return res.status(200).json({success: true, message:"Successfully updated case" , updateCase});
-    }catch(err){
-        return res.status(500).json({ success: false, message: err.message });
+        return res.status(200).json({success: true, message: "Successfully updated case", updateCase});
+    } catch (err) {
+        return res.status(500).json({success: false, message: err.message});
     }
 
 }
 
-exports.deleteCase = async (req,res)=>{
-    try{
+exports.deleteCase = async (req, res) => {
+    try {
         const deletedCase = await Case.findByIdAndDelete(req.params.id);
-        if(!deletedCase){
-            return res.status(404).json({success: false, message:"No such case"});
+        if (!deletedCase) {
+            return res.status(404).json({success: false, message: "No such case"});
         }
-        return res.status(200).json({success: true, message:"Successfully deleted case" , deletedCase});
-    }catch(err){
+        return res.status(200).json({success: true, message: "Successfully deleted case", deletedCase});
+    } catch (err) {
         return res.status(500).json({success: false, message: err.message})
     }
 }
 
 exports.getCaseInfo = async (req, res) => {
-    try{
+    try {
         const selectedCase = await Case.findById(req.params.id)
-        if(!selectedCase){
-            return res.status(404).json({success: false, message:"No such case"});
+        if (!selectedCase) {
+            return res.status(404).json({success: false, message: "No such case"});
         }
         return res.status(200).json({success: true, selectedCase});
-    }catch(err){
+    } catch (err) {
         return res.status(404).json({success: false, message: err.message});
     }
 }
@@ -63,6 +65,39 @@ exports.getAllCases = async (req, res) => {
         const cases = await Case.find();
         return res.status(200).json(cases);
     } catch (err) {
-        return res.status(500).json({ message: err.message });
+        return res.status(500).json({message: err.message});
     }
 };
+
+
+exports.searchCases = async (req, res) => {
+    try {
+        const query = req.query.q;
+
+        if (!query || query.trim() === "") {
+            return res.status(400).json({success: false, message: "Search query is required and cannot be empty"});
+        }
+
+        if (!query) {
+            return res.status(400).json({success: false, message: "Search query is required"});
+        }
+
+        const cases = await Case.find({
+            $or: [
+                {title: {$regex: query, $options: "i"}},
+                {description: {$regex: query, $options: "i"}}
+            ]
+        });
+
+
+        if (!cases.length) {
+            return res.status(404).json({success: false, message: "No such cases found"});
+        }
+
+        return res.status(200).json({success: true, cases: cases});
+
+
+    } catch (err) {
+        return res.status(500).json({success: false, message: err.message});
+    }
+}
--- routes/caseRoutes.js
+++ routes/caseRoutes.js
@@ -3,10 +3,11 @@ const router = express.Router();
 const caseController = require('../controllers/caseController');
 const authMiddleware = require('../middlewares/authMiddleware');
 
-router.post('/create', authMiddleware,caseController.createCase);
+router.post('/create', authMiddleware, caseController.createCase);
 router.get('/all', caseController.getAllCases)
-router.put('/update/:id', authMiddleware,caseController.updateCase);
-router.delete('/delete/:id', authMiddleware,caseController.deleteCase);
-router.get('/case/:id', authMiddleware,caseController.getCaseInfo)
+router.put('/update/:id', authMiddleware, caseController.updateCase);
+router.delete('/delete/:id', authMiddleware, caseController.deleteCase);
+router.get('/case/:id',  caseController.getCaseInfo)
+router.get('/search' , caseController.searchCases)
 
 module.exports = router;
